583f1532e5e3f0561a7e42f818602c3d43489cb9
Sudipta Mukherjee <sudipto80@yahoo.com>
2023-03-23 21:31:31 +0530

    formatting

--- CustomPrefCentre/CustomPrefCentreLibTests/Program.fs
+++ CustomPrefCentre/CustomPrefCentreLibTests/Program.fs
@@ -8,7 +8,10 @@ open System
 
 
 
-type UserChoice = { UserName : string; Choice : UserChoice option}
+type UserChoice =
+    { UserName: string
+      Choice: UserChoice option }
+
 [<Tests>]
 let reportGenerationTests =
     testList
@@ -17,7 +20,7 @@ let reportGenerationTests =
               let path = "..\..\..\TestData\choices.txt"
               let startDate = new DateTime(2018, 4, 1)
               let howManyDays = 13
-              let report = CustomPrefCentreLib.Facade.generateReport path startDate howManyDays
+              let report = generateReport path startDate howManyDays
 
               Expect.equal 14 report.Length "Expected 14 rows in the report."
               Expect.equal report.[0].CustomerNames [| "A" |] "Expected A to be present on the report but not found."
@@ -37,28 +40,44 @@ let reportGenerationTests =
                       || row.Date.DayOfWeek = DayOfWeek.Friday)
                   |> List.forall (fun row -> row.CustomerNames |> Array.contains "C")
 
-              Expect.isTrue C_Gets_Only_On_Tuesdays_And_Fridays "C is expected to get marketing material only on Tuesdays and Fridays"
+              Expect.isTrue
+                  C_Gets_Only_On_Tuesdays_And_Fridays
+                  "C is expected to get marketing material only on Tuesdays and Fridays"
 
 
               //Validating the whole report
               //One row at a time
               let expectedReport =
-                  [ { Date = DateTime.Parse("4/1/2018");CustomerNames = [| "A" |]};
-                    { Date = DateTime.Parse("4/2/2018");CustomerNames = [| "A" |]};
-                    { Date = DateTime.Parse("4/3/2018");CustomerNames = [| "A"; "C" |]};
-                    { Date = DateTime.Parse("4/4/2018");CustomerNames = [| "A" |]};
-                    { Date = DateTime.Parse("4/5/2018");CustomerNames = [| "A" |]};
-                    { Date = DateTime.Parse("4/6/2018");CustomerNames = [| "A"; "C" |]};
-                    { Date = DateTime.Parse("4/7/2018");CustomerNames = [| "A" |]};
-                    { Date = DateTime.Parse("4/8/2018");CustomerNames = [| "A" |]};
-                    { Date = DateTime.Parse("4/9/2018");CustomerNames = [| "A" |]};
-                    { Date = DateTime.Parse("4/10/2018");CustomerNames = [| "A"; "C"; "B" |]};
-                    { Date = DateTime.Parse("4/11/2018");CustomerNames = [| "A" |]};
-                    { Date = DateTime.Parse("4/12/2018");CustomerNames = [| "A" |]};
-                    { Date = DateTime.Parse("4/13/2018");CustomerNames = [| "A"; "C" |]};
-                    { Date = DateTime.Parse("4/14/2018");CustomerNames = [| "A" |]} ]
+                  [ { Date = DateTime.Parse("4/1/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/2/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/3/2018")
+                      CustomerNames = [| "A"; "C" |] }
+                    { Date = DateTime.Parse("4/4/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/5/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/6/2018")
+                      CustomerNames = [| "A"; "C" |] }
+                    { Date = DateTime.Parse("4/7/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/8/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/9/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/10/2018")
+                      CustomerNames = [| "A"; "C"; "B" |] }
+                    { Date = DateTime.Parse("4/11/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/12/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/13/2018")
+                      CustomerNames = [| "A"; "C" |] }
+                    { Date = DateTime.Parse("4/14/2018")
+                      CustomerNames = [| "A" |] } ]
 
-              Expect.equal expectedReport report "Something wrong in the report"
+              Expect.equal report expectedReport "Something wrong in the report"
 
 
           }
@@ -68,9 +87,9 @@ let reportGenerationTests =
 
               let startDate = new DateTime(2018, 4, 1)
               let howManyDays = 13
-              let report = CustomPrefCentreLib.Facade.generateReport path startDate howManyDays
+              let report = generateReport path startDate howManyDays
 
-              Expect.equal 14 report.Length "Expected 14 rows in the report."
+              Expect.equal report.Length 14 "Expected 14 rows in the report."
 
               Expect.isTrue
                   (report
@@ -83,27 +102,41 @@ let reportGenerationTests =
 
               let startDate = new DateTime(2018, 4, 1)
               let howManyDays = 13
-              let report = CustomPrefCentreLib.Facade.generateReport path startDate howManyDays
+              let report = generateReport path startDate howManyDays
 
               let expectedReport =
-                  [ { Date = DateTime.Parse("4/1/2018");CustomerNames = [| "A" |]};
-                    { Date = DateTime.Parse("4/2/2018");CustomerNames = [| "A"; "B" |]};
-                    { Date = DateTime.Parse("4/3/2018");CustomerNames = [| "A"; "B"; "C" |]};
-                    { Date = DateTime.Parse("4/4/2018");CustomerNames = [||]};
-                    { Date = DateTime.Parse("4/5/2018");CustomerNames = [||]};
-                    { Date = DateTime.Parse("4/6/2018");CustomerNames = [||]};
-                    { Date = DateTime.Parse("4/7/2018");CustomerNames = [||]};
-                    { Date = DateTime.Parse("4/8/2018");CustomerNames = [| "A" |]};
-                    { Date = DateTime.Parse("4/9/2018");CustomerNames = [| "A"; "B" |]};
-                    { Date = DateTime.Parse("4/10/2018");CustomerNames = [| "A"; "B"; "C" |]};
-                    { Date = DateTime.Parse("4/11/2018");CustomerNames = [||]};
-                    { Date = DateTime.Parse("4/12/2018");CustomerNames = [||]};
-                    { Date = DateTime.Parse("4/13/2018");CustomerNames = [||]};
-                    { Date = DateTime.Parse("4/14/2018");CustomerNames = [||]} ]
+                  [ { Date = DateTime.Parse("4/1/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/2/2018")
+                      CustomerNames = [| "A"; "B" |] }
+                    { Date = DateTime.Parse("4/3/2018")
+                      CustomerNames = [| "A"; "B"; "C" |] }
+                    { Date = DateTime.Parse("4/4/2018")
+                      CustomerNames = [||] }
+                    { Date = DateTime.Parse("4/5/2018")
+                      CustomerNames = [||] }
+                    { Date = DateTime.Parse("4/6/2018")
+                      CustomerNames = [||] }
+                    { Date = DateTime.Parse("4/7/2018")
+                      CustomerNames = [||] }
+                    { Date = DateTime.Parse("4/8/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/9/2018")
+                      CustomerNames = [| "A"; "B" |] }
+                    { Date = DateTime.Parse("4/10/2018")
+                      CustomerNames = [| "A"; "B"; "C" |] }
+                    { Date = DateTime.Parse("4/11/2018")
+                      CustomerNames = [||] }
+                    { Date = DateTime.Parse("4/12/2018")
+                      CustomerNames = [||] }
+                    { Date = DateTime.Parse("4/13/2018")
+                      CustomerNames = [||] }
+                    { Date = DateTime.Parse("4/14/2018")
+                      CustomerNames = [||] } ]
 
-              Expect.equal 14 report.Length "Expected 14 rows in the report."
+              Expect.equal report.Length 14 "Expected 14 rows in the report."
 
-              Expect.equal expectedReport report "Something wrong in the report"
+              Expect.equal report expectedReport "Something wrong in the report"
           }
 
 
@@ -111,28 +144,42 @@ let reportGenerationTests =
               let path = "..\..\..\TestData\duplicate_choices.txt"
               let startDate = new DateTime(2018, 4, 1)
               let howManyDays = 13
-              let report = CustomPrefCentreLib.Facade.generateReport path startDate howManyDays
+              let report = generateReport path startDate howManyDays
 
 
               //Validating the whole report
               //One row at a time
               let expectedReport =
-                  [ { Date = DateTime.Parse("4/1/2018");CustomerNames = [| "A" |]};
-                    { Date = DateTime.Parse("4/2/2018"); CustomerNames = [| "A" |]};
-                    { Date = DateTime.Parse("4/3/2018"); CustomerNames =  [| "A"; "C" |]};
-                    { Date = DateTime.Parse("4/4/2018"); CustomerNames =  [| "A" |]};
-                    { Date = DateTime.Parse("4/5/2018"); CustomerNames =  [| "A" |]};
-                    { Date = DateTime.Parse("4/6/2018"); CustomerNames =  [| "A"; "C" |]};
-                    { Date = DateTime.Parse("4/7/2018"); CustomerNames =  [| "A" |]};
-                    { Date = DateTime.Parse("4/8/2018"); CustomerNames =  [| "A" |]};
-                    { Date = DateTime.Parse("4/9/2018"); CustomerNames =  [| "A" |]};
-                    { Date = DateTime.Parse("4/10/2018"); CustomerNames =  [| "A"; "C"; "B" |]};
-                    { Date = DateTime.Parse("4/11/2018"); CustomerNames =  [| "A" |]};
-                    { Date = DateTime.Parse("4/12/2018"); CustomerNames =  [| "A" |]};
-                    { Date = DateTime.Parse("4/13/2018"); CustomerNames =  [| "A"; "C" |]};
-                    { Date = DateTime.Parse("4/14/2018"); CustomerNames =  [| "A" |]} ]
+                  [ { Date = DateTime.Parse("4/1/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/2/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/3/2018")
+                      CustomerNames = [| "A"; "C" |] }
+                    { Date = DateTime.Parse("4/4/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/5/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/6/2018")
+                      CustomerNames = [| "A"; "C" |] }
+                    { Date = DateTime.Parse("4/7/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/8/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/9/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/10/2018")
+                      CustomerNames = [| "A"; "C"; "B" |] }
+                    { Date = DateTime.Parse("4/11/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/12/2018")
+                      CustomerNames = [| "A" |] }
+                    { Date = DateTime.Parse("4/13/2018")
+                      CustomerNames = [| "A"; "C" |] }
+                    { Date = DateTime.Parse("4/14/2018")
+                      CustomerNames = [| "A" |] } ]
 
-              Expect.equal expectedReport report "Something wrong in the report"
+              Expect.equal report expectedReport "Something wrong in the report"
           } ]
     |> testLabel "Report tests"
 
@@ -143,23 +190,31 @@ let parsingTests =
         "Parsing tests"
         [ test "Parsing simple choice file" {
               let path = "..\..\..\TestData\choices.txt"
-              let choices = CustomPrefCentreLib.ChoiceParser.getUserChoices path 
-                          
-              Expect.equal choices.[0].UserName "A"  "Expected A at the start "
-              Expect.equal choices.[0].Choice.Value Everyday  "A has chosen Everyday ."
+              let choices = CustomPrefCentreLib.ChoiceParser.getUserChoices path
 
-              Expect.equal choices.[1].UserName "B"  "Second was B"
-              Expect.equal choices.[1].Choice.Value (Day(9))  "B selected every 10 days."
+              Expect.equal choices.[0].UserName "A" "Expected A at the start "
+              Expect.equal choices.[0].Choice.Value Everyday "A has chosen Everyday ."
 
-              Expect.equal choices.[2].UserName "C"  "C was at the end "
-              Expect.equal choices.[2].Choice.Value (DaysOfWeek([|DayOfWeek.Tuesday;DayOfWeek.Friday|]))  "C selected Tue and Fri."
+              Expect.equal choices.[1].UserName "B" "Second was B"
+              Expect.equal choices.[1].Choice.Value (Day(9)) "B selected every 10 days."
+
+              Expect.equal choices.[2].UserName "C" "C was at the end "
+
+              Expect.equal
+                  choices.[2].Choice.Value
+                  (DaysOfWeek(
+                      [| DayOfWeek.Tuesday
+                         DayOfWeek.Friday |]
+                  ))
+                  "C selected Tue and Fri."
           }
 
-        ]
+          ]
     |> testLabel "Parsing tests"
 
 [<EntryPoint>]
-let main args = 
-   runTestsWithCLIArgs [] args reportGenerationTests |> ignore 
-   runTestsWithCLIArgs [] args parsingTests 
- 
+let main args =
+    runTestsWithCLIArgs [] args reportGenerationTests
+    |> ignore
+
+    runTestsWithCLIArgs [] args parsingTests
